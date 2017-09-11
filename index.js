f21bd418bba75a6457a309ef5b3660b6ad951a45
xmhscratch <xmhscratch@gmail.com>
2017-09-11 21:14:36 +0700

    update

--- index.js
+++ index.js
@@ -19,14 +19,10 @@ system
     .then(($db) => {
         global.$db = $db
 
-        // $db.createMember()
-         // .then(console.log)
-        // $db.getMember(
-        //     '1b3d6c7b-295c-4e00-884c-dde01a763d2f'
-        // ).then(console.log)
-        // $db.removeMember(
-        //     '1b3d6c7b-295c-4e00-884c-dde01a763d2f'
-        // ).then(console.log)
+        // $db.createMember().then(console.log)
+        // const tenantId = 'fac65886-3ca0-4706-af00-926d34472799'
+        // $db.getMember(tenantId).then(console.log)
+        // $db.removeMember(tenantId).then(console.log)
 
         $server.start()
         $server.autoRefresh()
